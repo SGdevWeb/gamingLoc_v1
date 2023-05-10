b5414334dd5bc44d6cc75436e8c4918e7a30c660
Nicolas Beun <nicolas.beun@gmail.com>
2023-05-10 13:57:42 +0200

    Constructor & Getter/Setter in LanguageGame and changing name in CamelCase

--- src/main/java/dao/entities/Editor.java
+++ src/main/java/dao/entities/Editor.java
@@ -2,20 +2,20 @@ package dao.entities;
 
 public class Editor {
 
-    private int editor_id;
+    private int editorId;
     private String name;
 
-    public Editor(int editor_id, String name) {
-        this.editor_id = editor_id;
+    public Editor(int editorId, String name) {
+        this.editorId = editorId;
         this.name = name;
     }
 
-    public int getEditor_id() {
-        return editor_id;
+    public int getEditorId() {
+        return editorId;
     }
 
-    public void setEditor_id(int editor_id) {
-        this.editor_id = editor_id;
+    public void setEditorId(int editorId) {
+        this.editorId = editorId;
     }
 
     public String getName() {
--- src/main/java/dao/entities/LanguageGame.java
+++ src/main/java/dao/entities/LanguageGame.java
@@ -1,4 +1,28 @@
 package dao.entities;
 
 public class LanguageGame {
+
+    private int gameId;
+    private String acronym;
+
+    public LanguageGame(int gameId, String acronym) {
+        this.gameId = gameId;
+        this.acronym = acronym;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
 }
