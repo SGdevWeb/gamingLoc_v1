0570874d6d72db555640736cb8109918fd2fee32
Nicolas Beun <nicolas.beun@gmail.com>
2023-05-10 13:49:31 +0200

    Constructor & Getter/Setter in Languages

--- src/main/java/dao/entities/Languages.java
+++ src/main/java/dao/entities/Languages.java
@@ -2,4 +2,27 @@ package dao.entities;
 
 public class Languages {
 
+    private String acronym;
+    private String name;
+
+    public Languages(String acronym, String name) {
+        this.acronym = acronym;
+        this.name = name;
+    }
+
+    public String getAcronym() {
+        return acronym;
+    }
+
+    public void setAcronym(String acronym) {
+        this.acronym = acronym;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
