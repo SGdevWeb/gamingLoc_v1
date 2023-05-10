fbb2ec38ef1c6e8e6e4be6bd1c8929a5ca03f0cc
Samuel Gustin <samuel.gustin.dev@gmail.com>
2023-05-10 12:26:15 +0200

    Added Customer entity

--- src/main/java/dao/entities/Customer.java
+++ src/main/java/dao/entities/Customer.java
@@ -2,4 +2,75 @@ package dao.entities;
 
 public class Customer {
 
+    int customerId;
+    String firstName;
+    String lastName;
+    String password;
+    String email;
+    String phoneNumber;
+    int addressId;
+
+    public Customer(String firstName, String lastName, String password, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
 }
