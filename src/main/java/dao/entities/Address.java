d9944c564bc8896bba9992fa4de7a2a0579d7374
Samuel Gustin <samuel.gustin.dev@gmail.com>
2023-05-10 13:48:14 +0200

    Added Address entity

---
+++ src/main/java/dao/entities/Address.java
@@ -0,0 +1,40 @@
+package dao.entities;
+
+public class Address {
+
+    private int addressId;
+    private String streetAddress;
+    private String complementaryAddress;
+    private City city;
+
+    public Address(String streetAddress, String complementaryAddress, City city) {
+        this.streetAddress = streetAddress;
+        this.complementaryAddress = complementaryAddress;
+        this.city = city;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getComplementaryAddress() {
+        return complementaryAddress;
+    }
+
+    public void setComplementaryAddress(String complementaryAddress) {
+        this.complementaryAddress = complementaryAddress;
+    }
+
+}
